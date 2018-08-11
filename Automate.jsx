7573dbf76555bb48c628dda6936d11a01104ddb7
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2018-08-12 02:12:21 +0530

    UI change

---
+++ Automate.jsx
@@ -0,0 +1,112 @@
+// Photoshop_CC_2015
+// Automate "Name" & "POR" layers from a "CSV file" and save as "JPG file"
+// Automate all JPGs to A3 doc each with 8 JPGs and saving them as "JPG file"
+
+// Layer structure Group name : "NAME" & Layer name : "name"
+//				   Group name : "POR" & Layer name : "por"
+
+// Working document
+var doc = app.activeDocument;
+
+// Getting name layer
+// var NameGroup = doc.layerSets.getByName('NAME');
+// var NameLayer = NameGroup.layers.getByName('name');
+var NameLayer = doc.layers.getByName('name');
+
+// Getting por layer
+// var PORGroup = doc.layerSets.getByName('POR');
+// var PORLayer = PORGroup.layers.getByName('por');
+var PORLayer = doc.layers.getByName('por');
+
+// Function to save as "jpg" file in "/img/" directory
+function SaveJPEG(file_name) {
+	var file = new File(doc.path + '/' + 'imgs' + '/' + file_name + '.jpg');
+	var opts = new JPEGSaveOptions();
+	opts.quality = 10;
+	doc.saveAs(file, opts, true);
+}
+
+// Moves to its specific position in an order
+function MoveLayerTo(fLayer, num) {
+	var fX = 10.5*(num%4);	// X position (top left anchor)
+	var fY = 13.5;	// Y position (top left anchor)
+	if (num < 4) fY = 0;
+	var Position = fLayer.bounds;
+	Position[0] = fX - Position[0];
+	Position[1] = fY - Position[1];
+  	fLayer.translate(-Position[0], -Position[1]);
+}
+
+// Reading CSV file by taking the file as user input
+var csvFile = File.openDialog("Open CSV File","comma-delimited(*.csv):*.csv;"); 
+csvFile.open('r');
+var csvString = csvFile.read();
+csvFile.close();
+// Splitting into rows
+csvString = csvString.split('\n');
+
+// Folder to contain IDs
+var folder1 = Folder(doc.path + '/' + "imgs");
+//Check if it exist, if not create it.
+if(!folder1.exists) folder1.create();
+
+// Automation; edit; save;
+for( var s = 1; s < csvString.length; s++ ) {	// s=1 to ignone heading
+var lineData = csvString[s].split(",");			// split based on comma seperation
+NameLayer.textItem.contents = lineData[0];		// first element 'name'
+PORLayer.textItem.contents = lineData[1];		// second element 'por'
+SaveJPEG(lineData[1] + '-' + lineData[0] + '-' + s);		// file name 'por-name-uniqueID'
+}
+
+alert("Amigo!" + '\n' + "Done creating " + (csvString.length-1) + " files.");
+
+// Creating new PSD file
+var newdoc = app.documents.add(42.0, 29.7, 300, "IDs", NewDocumentMode.CMYK);
+
+// Use the path to the application and append the samples folder 
+var samplesFolder = Folder(doc.path + "/imgs/");
+// Get all the jpgs in the folder
+var fileListX = samplesFolder.getFiles("*.JPG");
+var fileList = fileListX.sort();
+
+// Rounding off to next integer
+if (Math.round(fileList.length/8) < fileList.length/8)
+	numberRou = Math.round(fileList.length/8) + 1;
+else 
+	numberRou = Math.round(fileList.length/8);
+
+// Folder to contain A3 sheets
+var folder2 = Folder(doc.path + '/' + "A3");
+// Check if it exist, if not create it.
+if(!folder2.exists) folder2.create();
+
+count = 0;
+for (var i = 0; i < numberRou; i++) {
+	// Itterating through each file
+	nJPG = 8
+	if (numberRou-1 == i && fileList.length%8 != 0) nJPG = fileList.length%8
+	for (var e = 0; e < nJPG; e++) {
+		myFile = fileList[count];
+		count += 1;
+		var curr_file = app.open(myFile);
+		curr_file.selection.selectAll();
+		curr_file.selection.copy();
+		curr_file.close(SaveOptions.DONOTSAVECHANGES);
+		newdoc.paste();
+		MoveLayerTo(newdoc.layers[0], e);
+	}
+	var mfile = new File(doc.path + '/' + 'A3' + '/' + 'Set-' + (i+1) + '.jpg');
+	var mopts = new JPEGSaveOptions();
+	mopts.quality = 10;
+	newdoc.saveAs(mfile, mopts, true);
+	for (var j = 0; j < nJPG; j++) newdoc.layers[0].remove();
+}
+
+// Closing A3 document
+newdoc.close(SaveOptions.DONOTSAVECHANGES);
+
+// Closing application
+if (confirm('All Done!' + '\n' +  'Wanna quit PS ?')) {		// User input
+    var idquit = charIDToTypeID( "quit" );
+	executeAction( idquit, undefined, DialogModes.ALL );
+}
---
+++ README.md
@@ -0,0 +1,66 @@
+# PS Automator
+Using JavaScript to automate monotonous operations in Photoshop.
+
+## Getting Started
+
+Any version of Photoshop CC will work fine.
+```
+Note : Photoshop CC 2015 was used while developing this application.
+```
+### Prerequisites
+
+* Knowledge of Layers and Groups in photoshop.
+* Basic syntax of JavaScript.
+
+## Steps to follow
+
+### Step 1 : Creating CSV file
+Create a CSV file with two columns.
+* 1st columns - NAME
+* 2nd columns - POST
+
+### Step 2 : Structuring PSD layers
+Create two layer, Name them as follows:
+* 1st layer named "name".
+* 2nd layer named "por".
+```
+Note : Make sure that this two layers are not enclosed in any groups.
+```
+### Step 3 : Run the script
+Run the script and leave the monotonous stuff to it! 
+
+## Process overview 
+Process done by the script.
+
+* Creates "imgs" folder in the same directory as the "Template.psd".
+* Saves all "JPGs" to imgs folder.
+* Creates "A3" folder in the same directory as the "Template.psd".
+* Stores all images in "imgs" folder to array.
+* Gets images from this arrays and compiles 8 images into one set.
+* Saves this A3 image set to "A3" folder.
+
+
+## Visual Guid
+
+* Open Template.psd follow the path : File > Scripts > Browse...
+  ![image](/README/1.png)
+  
+* Load the Script (.jsx file)
+  ![image](/README/2.png)
+  
+* User input : CSV file with contains all the Names and Posts.
+  ![image](/README/3.png)
+  
+* Finish alert!
+  ![image](/README/4.png)
+  
+  
+## Video Tutorial 
+
+* Layer structure, Ignore the layer stucture shown in the video.
+  ![image](/README/5.png)
+
+  [![Watch the video](/README/Thumbnail.jpg)](https://youtu.be/yl-frsRGyPM)
+
+
+### And thats how its done!
